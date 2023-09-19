d3c00b1bce1b42390da478d978abd9c73c88ef19
Thierry Lelegard <thierry@lelegard.fr>
2023-09-19 16:38:23 +0200

    Simplify demo counters

--- apps/demo-counters.cpp
+++ apps/demo-counters.cpp
@@ -21,20 +21,39 @@
 #include <clocale>
 
 // Display a timer access permission.
-void print_access(const char* name, bool enabled)
+void PrintAccess(const char* name, bool enabled)
 {
     std::cout << "EL0 access to " << name << " registers: " << (enabled ? "enabled" : "trapped") << std::endl;
+}
+
+// Get timer counter.
+inline csr_u64_t GetVirtualCounter()
+{
+    csr_u64_t counter = 0;
+    asm volatile("isb \n mrs %0, cntvct_el0" : "=r" (counter));
+    return counter;
+}
+inline csr_u64_t GetPhysicalCounter()
+{
+    csr_u64_t counter = 0;
+    asm volatile("isb \n mrs %0, cntpct_el0" : "=r" (counter));
+    return counter;
 }
 
 // Format a counter value. Including time difference with previous counter, ifthere is one.
 std::string CounterString(csr_u64_t counter, csr_u64_t previous = 0, csr_u64_t freq = 0)
 {
-    std::string s(ToHexa(counter) + Format(" (%'" PRIu64, counter));
-    if (previous > 0 && previous < counter && freq > 0) {
-        s += Format(" => %'" PRIu64 " micro-sec", ((counter - previous) * 1000000) / freq);
+    if (counter == 0) {
+        return "disabled";
     }
-    s += ")";
-    return s;
+    else {
+        std::string s(ToHexa(counter) + Format(" (%'" PRIu64, counter));
+        if (previous > 0 && previous < counter && freq > 0) {
+            s += Format(" => %'" PRIu64 " micro-sec", ((counter - previous) * 1000000) / freq);
+        }
+        s += ")";
+        return s;
+    }
 }
 
 // Program entry point
@@ -58,10 +77,10 @@ int main(int argc, char* argv[])
     const bool el0vcten = (cntkctl & (1 << 1)) != 0;
     const bool el0pcten = (cntkctl & 1) != 0;
 
-    print_access("physical timer", el0pten);
-    print_access("virtual timer", el0vten);
-    print_access("physical counter", el0pcten);
-    print_access("virtual counter", el0vcten);
+    PrintAccess("physical timer", el0pten);
+    PrintAccess("virtual timer", el0vten);
+    PrintAccess("physical counter", el0pcten);
+    PrintAccess("virtual counter", el0vcten);
 
     std::cout << "Counter event stream: ";
     if (cntkctl & (1 << 2)) {
@@ -78,59 +97,39 @@ int main(int argc, char* argv[])
     std::cout << std::endl << "Counter frequency: " << Format("%'" PRIu64, freq) << " Hz" << std::endl;
 
     // Get physical counters.
-    csr_u64_t el0_phys1 = 0, el1_phys1 = 0;
-    std::cout << std::endl << "EL0 physical counter: ";
-    if (el0pcten) {
-        asm volatile("isb \n mrs %0, cntpct_el0" : "=r" (el0_phys1));
-        std::cout << CounterString(el0_phys1);
-    }
-    else {
-        std::cout << "disabled";
-    }
+    csr_u64_t el0_phys1 = el0pcten ? GetPhysicalCounter() : 0;
+    csr_u64_t el1_phys1 = 0;
     regs.read(CSR_REGID_CNTPCT_EL0, el1_phys1);
-    std::cout << std::endl << "EL1 physical counter: " << CounterString(el1_phys1) << std::endl;
+    std::cout << std::endl
+              << "EL0 physical counter: " << CounterString(el0_phys1) << std::endl
+              << "EL1 physical counter: " << CounterString(el1_phys1) << std::endl;
 
     // Get virtual counters.
-    csr_u64_t el0_virt1 = 0, el1_virt1 = 0;
-    std::cout << std::endl << "EL0 virtual counter:  ";
-    if (el0vcten) {
-        asm volatile("isb \n mrs %0, cntvct_el0" : "=r" (el0_virt1));
-        std::cout << CounterString(el0_virt1);
-    }
-    else {
-        std::cout << "disabled";
-    }
+    csr_u64_t el0_virt1 = el0vcten ? GetVirtualCounter() : 0;
+    csr_u64_t el1_virt1 = 0;
     regs.read(CSR_REGID_CNTVCT_EL0, el1_virt1);
-    std::cout << std::endl << "EL1 virtual counter:  " << CounterString(el1_virt1) << std::endl;
+    std::cout << std::endl
+              << "EL0 virtual counter:  " << CounterString(el0_virt1) << std::endl
+              << "EL1 virtual counter:  " << CounterString(el1_virt1) << std::endl;
 
     std::cout << std::endl << "Waiting one second ..." << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
     // Get physical counters again.
-    csr_u64_t el0_phys2 = 0, el1_phys2 = 0;
-    std::cout << std::endl << "EL0 physical counter: ";
-    if (el0pcten) {
-        asm volatile("isb \n mrs %0, cntpct_el0" : "=r" (el0_phys2));
-        std::cout << CounterString(el0_phys2, el0_phys1, freq);
-    }
-    else {
-        std::cout << "disabled";
-    }
+    csr_u64_t el0_phys2 = el0pcten ? GetPhysicalCounter() : 0;
+    csr_u64_t el1_phys2 = 0;
     regs.read(CSR_REGID_CNTPCT_EL0, el1_phys2);
-    std::cout << std::endl << "EL1 physical counter: " << CounterString(el1_phys2, el1_phys1, freq) << std::endl;
+    std::cout << std::endl
+              << "EL0 physical counter: " << CounterString(el0_phys2, el0_phys1, freq) << std::endl
+              << "EL1 physical counter: " << CounterString(el1_phys2, el1_phys1, freq) << std::endl;
 
     // Get virtual counters again.
-    csr_u64_t el0_virt2 = 0, el1_virt2 = 0;
-    std::cout << std::endl << "EL0 virtual counter:  ";
-    if (el0vcten) {
-        asm volatile("isb \n mrs %0, cntvct_el0" : "=r" (el0_virt2));
-        std::cout << CounterString(el0_virt2, el0_virt1, freq);
-    }
-    else {
-        std::cout << "disabled";
-    }
+    csr_u64_t el0_virt2 = el0vcten ? GetVirtualCounter() : 0;
+    csr_u64_t el1_virt2 = 0;
     regs.read(CSR_REGID_CNTVCT_EL0, el1_virt2);
-    std::cout << std::endl << "EL1 virtual counter:  " << CounterString(el1_virt2, el1_virt1, freq) << std::endl;
+    std::cout << std::endl
+              << "EL0 virtual counter:  " << CounterString(el0_virt2, el0_virt1, freq) << std::endl
+              << "EL1 virtual counter:  " << CounterString(el1_virt2, el1_virt1, freq) << std::endl;
 
     return EXIT_SUCCESS;
 }
